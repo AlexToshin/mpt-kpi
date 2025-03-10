472148db324f502fd134a8c0ba9337e1d7e0e56a
Lesha <aleksvkv95@gmail.com>
2025-03-10 04:14:02 +0300

    Revert "add endpoint certificates -> Crtiteria"
    This reverts commit 78e49724f90dae9cbb3eb1f3c9d97e60289fc5f9.

--- backend/resources/criteries_resource.py
+++ backend/resources/criteries_resource.py
@@ -1,32 +1,7 @@
 from flask_restful import Resource, reqparse
-from flask_login import login_required, current_user
-from services.certificates_generator import Certificate, certificates_list
 from services.criteries_generator import Criterion, criteries_list
 
-class UserCertificatesResource(Resource):
-    @login_required
-    def get(self):
-        # Параметры запроса: фильтрация по пользователю
-        parser = reqparse.RequestParser()
-        parser.add_argument('user_id', type=int, required=False, default=current_user.id)
-        args = parser.parse_args()
 
-        # Фильтрация сертификатов по user_id (эмуляция через чётность ID)
-        user_certificates = [cert for cert in certificates_list if cert.id % 2 == args['user_id'] % 2]
-
-        result = []
-        for cert in user_certificates:
-            criterion = next((c for c in criteries_list if c.id == cert.criterion_id), None)
-            if criterion:
-                result.append({
-                    "id": cert.id,
-                    "preview_url": cert.preview_url,
-                    "criterion": {
-                        "id": criterion.id,
-                        "name": criterion.name,
-                        "mark_from": criterion.mark_from,
-                        "mark_to": criterion.mark_to
-                    }
-                })
-
-        return result, 200
+class CriteriesResource(Resource):
+    def get(self) -> dict:
+        return [criterion._asdict() for criterion in criteries_list]
